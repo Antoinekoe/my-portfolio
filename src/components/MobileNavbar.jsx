c58297415c2ffd39740dcbf756d9c22893a5106e
Antoinekoe <antoine.koe@gmail.com>
2025-08-06 10:15:32 +0200

    Mobile navbar done

---
+++ src/components/MobileNavbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MobileNavbar = () => {
+  return (
+    <div className="absolute h-full w-full px-4 py-10 bg-[#0D1116] text-white">
+      <ul className="flex flex-col gap-10">
+        <li className="listItems">Expertise</li>
+        <li className="listItems">Histoire</li>
+        <li className="listItems">Projets</li>
+        <li className="listItems">Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MobileNavbar;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { Sun } from "lucide-react";
+import React, { useState } from "react";
+import { Menu, Sun, X } from "lucide-react";
+import MobileNavbar from "./MobileNavbar";
 
 const Navbar = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <div className="flex justify-between px-8 py-5 bg-[#0D1116] text-white">
-      <Sun className="cursor-pointer" />
-      <ul className="flex gap-4 text-lg">
-        <li>Expertise</li>
-        <li>Histoire</li>
-        <li>Projets</li>
-        <li>Contact</li>
-      </ul>
-    </div>
+    <>
+      <div className="flex justify-between px-8 py-5 bg-[#0D1116] text-white">
+        <Sun className="cursor-pointer" />
+        <Menu
+          className={`${isClicked ? "hidden" : ""} sm:hidden`}
+          onClick={() => setIsClicked(true)}
+        />
+        <X
+          className={`${isClicked ? "" : "hidden"} sm:hidden`}
+          onClick={() => setIsClicked(false)}
+        />
+        <ul className="hidden sm:flex gap-4 text-lg">
+          <li>Expertise</li>
+          <li>Histoire</li>
+          <li>Projets</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      {isClicked && <MobileNavbar />}
+    </>
   );
 };
 
